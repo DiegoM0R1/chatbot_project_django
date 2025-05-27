ab381a1ad2d09dac7d78d0b9e7ae0f552e0fc5cc
DiegoM0R1 <zuluetamori@gmail.com>
2025-05-27 15:37:51 -0500

    route update

--- chatbot_project_django/urls.py
+++ chatbot_project_django/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 # chatbot_project_django/urls.py
 from django.contrib import admin
 from django.urls import path, include # Add include
-
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chatbot/', include('chatbot_app.urls')), # Include your app's URLs under 'chatbot/' prefix
+    path('chatbot/', include('chatbot_app.urls')), 
+    path('', RedirectView.as_view(url='/chatbot/', permanent=False))# Include your app's URLs under 'chatbot/' prefix
     # You can also map it to root: path('', include('chatbot_app.urls')),
 ]
