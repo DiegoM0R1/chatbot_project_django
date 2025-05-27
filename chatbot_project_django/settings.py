a1ed304448c20d2bb905a704f1e9ea9afc00e499
DiegoM0R1 <zuluetamori@gmail.com>
2025-05-27 15:34:50 -0500

    allouhost update

--- chatbot_project_django/settings.py
+++ chatbot_project_django/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-e=j_#40uf^)y97_xxo3c&guvfax$@5-41ye54mry=m=25vspd_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'auradeco.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
